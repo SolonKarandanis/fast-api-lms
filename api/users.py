1d28ff6dc7d12ab7093ee5fa89db2c032f990ade
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-09-22 14:09:03 +0300

    Implemented users api

--- api/users.py
+++ api/users.py
@@ -1,23 +1,33 @@
+from os import stat
+from typing import Optional, List
+
 import fastapi
-from pydantic import BaseModel
-from typing import Optional,List
+from fastapi import Depends, HTTPException
+from sqlalchemy.orm import Session
+
+from db.db_setup import get_db
+from schemas.user import UserCreate, User
+from api.utils.users import get_user, get_user_by_email, get_users, create_user
 
 router = fastapi.APIRouter()
 
-class User(BaseModel):
-    email: str
-    is_active: bool
-    bio:Optional[str]
-
-@router.get("/users")
-async def get_users():
-    return {"message": "Hi"}
+@router.get("/users", response_model=List[User])
+async def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    users = get_users(db, skip=skip, limit=limit)
+    return users
 
 
-@router.get("/users/{id}")
-async def get_user(id: int):
-    return {"message": "Hi"}
+@router.post("/users", response_model=User, status_code=201)
+async def create_new_user(user: UserCreate, db: Session = Depends(get_db)):
+    db_user = get_user_by_email(db=db, email=user.email)
+    if db_user:
+        raise HTTPException(status_code=400, detail="Email is already registered")
+    return create_user(db=db, user=user)
 
-@router.post("/users")
-async def create_user():
-    return {"message": "Hi"}
+
+@router.get("/users/{user_id}", response_model=User)
+async def read_user(user_id: int, db: Session = Depends(get_db)):
+    db_user = get_user(db=db, user_id=user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return db_user
