84f724dd2ff29088d4f6a89b6aa88c0fc1a93652
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-09-22 09:36:34 +0300

    Small changes

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional,List
 
 app = FastAPI()
 
+
+class User(BaseModel):
+    email: str
+    is_active: bool
+    bio:Optional[str]
 
 @app.get("/users")
 async def get_users():
