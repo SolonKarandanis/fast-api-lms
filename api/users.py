9fc511a6927520062d5c4b15eb504558fef9848b
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-09-28 11:41:14 +0300

    Added logger

--- api/users.py
+++ api/users.py
@@ -3,6 +3,7 @@ from typing import Optional, List
 import fastapi
 from fastapi import Depends, HTTPException
 from sqlalchemy.orm import Session
+from logger import logger
 
 from db.db_setup import get_db
 from schemas.user import UserCreate, User
@@ -14,12 +15,14 @@ router = fastapi.APIRouter()
 @router.get("/users", response_model=List[User])
 async def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     users = get_users(db, skip=skip, limit=limit)
+    logger.info(f"---->read_users -> users: {users}")
     return users
 
 
 @router.post("/users", response_model=User, status_code=201)
 async def create_new_user(user: UserCreate, db: Session = Depends(get_db)):
     db_user = get_user_by_email(db=db, email=user.email)
+    logger.info(f"---->create_new_user -> db_user: {db_user}")
     if db_user:
         raise HTTPException(status_code=400, detail="Email is already registered")
     return create_user(db=db, user=user)
