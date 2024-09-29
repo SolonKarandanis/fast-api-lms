01bdb38aa56062505b9b4fe9f3e09e74d311ceb9
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-09-29 14:44:01 +0300

    Refactored code

--- api/song.py
+++ api/song.py
@@ -8,14 +8,14 @@ from db.models import Song, SongCreate
 router = fastapi.APIRouter()
 
 @router.get("/songs", response_model=list[Song])
-def get_songs(session: Session = Depends(get_session)):
+async def get_songs(session: Session = Depends(get_session)):
     result = session.execute(select(Song))
     songs = result.scalars().all()
     return [Song(name=song.name, artist=song.artist, id=song.id) for song in songs]
 
 
 @router.post("/songs")
-def add_song(song: SongCreate, session: Session = Depends(get_session)):
+async def add_song(song: SongCreate, session: Session = Depends(get_session)):
     song = Song(name=song.name, artist=song.artist)
     session.add(song)
     session.commit()
